30f59d722bd943f439ccda1a47fa6142dc875203
amaroguy <amaroguy@gmail.com>
2022-08-07 16:46:33 -0700

    changed origin again

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors())
 const server = http.createServer(app)
 const io = new Server(server,  {
     cors: {
-        origin: "http://amaro.tech",
+        origin: "https://amaro.tech",
         methods: ["GET", "POST"]
     }
 })
