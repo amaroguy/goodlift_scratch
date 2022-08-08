eddca6b08103c9470c9769678494afd9ca26e075
amaroguy <amaroguy@gmail.com>
2022-08-07 17:06:06 -0700

    sss

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors())
 const server = http.createServer(app)
 const io = new Server(server,  {
     cors: {
-        origin: "https://amaro.tech",
+        origin: "amaro.tech",
         methods: ["GET", "POST"]
     }
 })
